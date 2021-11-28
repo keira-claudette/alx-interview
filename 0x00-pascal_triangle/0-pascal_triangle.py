7f4a19cdf2373ba470fe0c84fab9625581fbe4cb
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-11-28 16:05:10 +0300

    initial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+def pascal_triangle(n):
+  '''  returns a list of lists of integers representing the Pascal’s triangle of n'''
+    if n <= 0:
+      return []
+    else:
+      left = 0
+      right = 0
+      lines = []
+      vagrant
